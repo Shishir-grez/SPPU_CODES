cbee348741d5f1c017da3eaf5d628c8c49261491
Shishir-grez <narkhedeshishir2003@gmail.com>
2022-12-28 23:03:55 +0530

    Added B4

---
+++ OOP/B4_1.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+class test
+{
+public:
+	void writedata();
+	void readdata();
+};
+void test::writedata()
+{
+ fstream fp;
+ char ch;
+ fp.open("it.txt",ios::out);
+ cin>>ch;
+ while (ch!='.')
+ {
+ fp.put(ch);
+ cin>>ch;
+ }
+fp.close();
+}
+void test::readdata()
+{
+	fstream fp;
+	char ch;
+	fp.open("it.txt",ios::in);
+ch=fp.get();
+while(!fp.eof())
+{
+cout<<ch;
+ch=fp.get(); 
+}
+fp.close();
+}
+int main()
+{
+  test ob;
+  int ch;
+  do
+  {
+   cout<<"\n1.Write\n2.Read";
+   cout<<"\nEnter your choice= ";
+   cin>>ch;
+   switch(ch)
+   {
+    case 1:
+	ob.writedata();
+    break;
+    case 2:
+	ob.readdata();
+    break;
+   }
+  }while(ch<3);
+}
